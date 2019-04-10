78f0e49437c09a38888a3eab2d0d47b1349d6f45
Benedikt Radtke <benediktradtke@gmail.com>
2019-04-10 02:10:39 +0200

    Adapt doms comments

--- EnoCore/Models/Database/LogEntry.cs
+++ EnoCore/Models/Database/LogEntry.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -14,12 +15,19 @@ namespace EnoCore.Models.Database
 
     public class CheckerLogMessage
     {
+        [JsonIgnore]
         public long Id { get; set; }
+        [JsonProperty("message")]
         public string Message { get; set; }
+        [JsonProperty("timestamp")]
         public DateTime Timestamp { get; set; }
+        [JsonProperty("severity")]
         public LogSeverity Severity { get; set; }
+        [JsonIgnore]
         public CheckerTask RelatedTask { get; set; }
+        [JsonProperty("runId")]
         public long RelatedTaskId { get; set; }
+        [JsonProperty("tag")]
         public string Origin { get; set; }
     }
 }
