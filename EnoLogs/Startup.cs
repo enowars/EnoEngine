d6c3e7a28b5ba7c7eb14f7519bc920a3ef882fb6
Benedikt Radtke <benediktradtke@gmail.com>
2019-04-11 21:28:37 +0200

    Always enable the dev exceptions page

--- EnoLogs/Startup.cs
+++ EnoLogs/Startup.cs
@@ -30,11 +30,7 @@ namespace EnoLogs
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
+            app.UseDeveloperExceptionPage();
             app.UseMvc();
         }
     }
