89db76fd2f5c1001b4323764940d1238e2c86929
Benedikt Radtke <benediktradtke@gmail.com>
2019-04-16 00:30:16 +0200

    Both writes for now

--- EnoCore/Logging.cs
+++ EnoCore/Logging.cs
@@ -58,6 +58,7 @@ namespace EnoCore
             message.Tool = Tool;
             message.Timestamp = DateTime.Now.ToString(); //TODO chose a date format
             Debug.WriteLine(JsonConvert.SerializeObject(message));
+            Console.WriteLine(JsonConvert.SerializeObject(message));
         }
     }
 }
