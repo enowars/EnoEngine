92fcbe122e006a055a432ef8618a87f90e98ce53
Benedikt Radtke <benediktradtke@gmail.com>
2019-10-26 23:10:31 +0200

    wörk

--- EnoCore/Logging/EnoStatistics.cs
+++ EnoCore/Logging/EnoStatistics.cs
@@ -22,13 +22,13 @@ namespace EnoCore.Logging
         {
             var message = new SubmissionBatchMessage(flagsProcessed,
                 okFlags, duplicateFlags, oldFlags, duration);
-            Queue.Enqueue(PREFIX + JsonConvert.ToString(message));
+            Queue.Enqueue(PREFIX + JsonConvert.SerializeObject(message));
         }
 
         public void CheckerTaskLaunchMessage(CheckerTask task)
         {
             var message = new CheckerTaskLaunchMessage(task);
-            Queue.Enqueue(PREFIX + JsonConvert.ToString(message));
+            Queue.Enqueue(PREFIX + JsonConvert.SerializeObject(message));
         }
     }
 
