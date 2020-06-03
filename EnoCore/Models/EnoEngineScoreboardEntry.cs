77305a0408d6575b143e69b3b27db1f82d807972
Benedikt Constantin Radtke <benediktradtke@mailbox.tu-berlin.de>
2020-06-03 20:24:34 +0200

    More longs

--- EnoCore/Models/EnoEngineScoreboardEntry.cs
+++ EnoCore/Models/EnoEngineScoreboardEntry.cs
@@ -16,16 +16,16 @@ namespace EnoCore.Models
         public double AttackPoints { get => Team.AttackPoints; }
         public double LostDefensePoints { get => Team.LostDefensePoints; }
         public double ServiceLevelAgreementPoints { get => Team.ServiceLevelAgreementPoints; }
-        public Dictionary<long, EnoEngineScoreboardEntryServiceDetails> ServiceDetails { get; set; }
+        public Dictionary<string, EnoEngineScoreboardEntryServiceDetails> ServiceDetails { get; set; }
 
 
         public EnoEngineScoreboardEntry(Team team, IEnumerable<ServiceStats> serviceStats)
         {
             Team = team;
-            ServiceDetails = new Dictionary<long, EnoEngineScoreboardEntryServiceDetails>();
+            ServiceDetails = new Dictionary<string, EnoEngineScoreboardEntryServiceDetails>();
             foreach (var service in serviceStats)
             {
-                ServiceDetails.Add(service.ServiceId, new EnoEngineScoreboardEntryServiceDetails(service));
+                ServiceDetails.Add(service.ServiceId.ToString(), new EnoEngineScoreboardEntryServiceDetails(service));
             }
         }
     }
