9f1d5647e98238de7f14ac9bb8d09621e5670257
Moritz Grothe <mgrothe1@mail.upb.de>
2020-06-15 15:58:04 +0200

    fixed generator script

--- generate_ctf_json.py
+++ generate_ctf_json.py
@@ -4,12 +4,15 @@ config = '''{
     "RoundLengthInSeconds": 60,
     "DnsSuffix": "eno.host",
     "TeamSubnetBytesLength": 6,
+    "FlagSigningKey": "ir7PRm0SzqzA0lmFyBfUv68E6Yb7cjbJDp6dummqwr0Od70Sar7P27HVY6oc8PuW",
+    "NoiseSigningKey": "cGSiyYn6VjTUxS7PZInBaCYW83KTaFJPq6zaWji0NGzJ6wpZMUDJKbgo8tkfT35w",
     "Teams": ['''
 
 for i in range(256):
     config += '''{
         "Id": '''+str(i+1)+''',
         "Name": "teamname'''+str(i+1)+'''",
+		"Address": "51.75.156.188",
         "TeamSubnet": "fd00:1337:'''+str(i+1)+'''::"
     },'''
 config = config[:-1]
