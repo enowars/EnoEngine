49cb5f3eb70ae914ff0071f0476895beb585dad4
Moritz Grothe <mgrothe1@mail.upb.de>
2020-06-18 13:58:26 +0200

    Removed newlines

--- EnoEngine/Program.cs
+++ EnoEngine/Program.cs
@@ -91,8 +91,6 @@ namespace EnoEngine
                     {
                         Console.WriteLine("Another Instance is already running");
                     }
-
-
                 }
                 finally
                 {
