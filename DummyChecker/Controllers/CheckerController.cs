bd3f9e8212e55a56374b8818aec74cf793181093
Benedikt Radtke <benediktradtke@gmail.com>
2020-07-15 18:56:34 +0200

    Add dummychecker

---
+++ DummyChecker/Controllers/CheckerController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using EnoCore.Models.Json;
+
+namespace GamemasterChecker.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    [Route("/service")]
+    public class CheckerController : Controller
+    {
+        [HttpPost]
+        [Route("/")]
+        public IActionResult Flag([FromBody] CheckerTaskMessage content)
+        {
+            return Ok("{ \"result\": \"OK\" }");
+        }
+    }
+}
