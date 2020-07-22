f3bddfaaeba36051ea4660b845c95e2a0ec1b956
Moritz Grothe <mgrothe1@mail.upb.de>
2020-07-23 01:18:05 +0200

    made stuff internal

--- DummyChecker/Startup.cs
+++ DummyChecker/Startup.cs
@@ -13,7 +13,7 @@ using Microsoft.Extensions.Logging;
 
 namespace DummyChecker
 {
-    public class Startup
+    internal class Startup
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
