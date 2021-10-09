d2ca5daf65e26bd5e2e2696070b2f5c507e37977
Benedikt Radtke <benediktradtke@gmail.com>
2021-10-09 17:55:48 +0200

    This could totally work

--- EnoFlagSink/EnoFlagSinkUtil.cs
+++ EnoFlagSink/EnoFlagSinkUtil.cs
@@ -9,22 +9,23 @@
     using System.Threading;
     using System.Threading.Tasks;
 
-    public class EnoFlagSinkUtils
+    public class EnoFlagSinkUtil
     {
         private const int MaxLineLength = 200;
 
-        public static async Task<bool> ReadLine(
+        public static async Task<bool> ReadLines(
             PipeReader pipeReader,
-            Func<ReadOnlySequence<byte>, Task> handler,
+            Func<ReadOnlySequence<byte>, Task<bool>> handler,
             CancellationToken token)
         {
-            while (true)
+            bool running = true;
+            while (running)
             {
                 ReadResult result = await pipeReader.ReadAsync(token);
                 ReadOnlySequence<byte> buffer = result.Buffer;
                 while (TryReadLine(ref buffer, out ReadOnlySequence<byte> line))
                 {
-                    await handler(line);
+                    running = await handler(line);
                 }
 
                 // Tell the PipeReader how much of the buffer has been consumed.
@@ -38,6 +39,8 @@
 
                 // TryReadLine has returned false, so the remaining buffer does not contain a \n.
                 // If the length is longer than a flag, somebody is sending bullshit!
+                // This is not the strict upper limit though, because the buffer might be bigger,
+                // and thus yield strings longer than this check.
                 if (buffer.Length > MaxLineLength)
                 {
                     return false;
