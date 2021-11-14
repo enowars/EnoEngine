7a3438f59d7e9cd118f7659edc14071f44a71409
Benedikt Radtke <benediktradtke@gmail.com>
2021-11-14 17:52:38 +0100

    C# 10 EnoCore

--- EnoCore/EnoCoreUtil.cs
+++ EnoCore/EnoCoreUtil.cs
@@ -1,34 +1,23 @@
-﻿namespace EnoCore
+﻿namespace EnoCore;
+public class EnoCoreUtil
 {
-    using System;
-    using System.Collections.Generic;
-    using System.IO;
-    using System.Linq;
-    using System.Text;
-    using System.Text.Json;
-    using System.Text.Json.Serialization;
-    using System.Threading.Tasks;
-
-    public class EnoCoreUtil
+    public const string DateTimeFormat = "yyyy-MM-ddTHH:mm:ss.fffZ";
+    public static readonly string DataDirectory = $"..{Path.DirectorySeparatorChar}data{Path.DirectorySeparatorChar}";
+    public static readonly JsonSerializerOptions CamelCaseEnumConverterOptions = new JsonSerializerOptions
     {
-        public const string DateTimeFormat = "yyyy-MM-ddTHH:mm:ss.fffZ";
-        public static readonly string DataDirectory = $"..{Path.DirectorySeparatorChar}data{Path.DirectorySeparatorChar}";
-        public static readonly JsonSerializerOptions CamelCaseEnumConverterOptions = new JsonSerializerOptions
-        {
-            AllowTrailingCommas = true,
-            ReadCommentHandling = JsonCommentHandling.Skip,
-            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-            Converters = { new JsonStringEnumConverter() },
-        };
+        AllowTrailingCommas = true,
+        ReadCommentHandling = JsonCommentHandling.Skip,
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        Converters = { new JsonStringEnumConverter() },
+    };
 
-        public static string GetCurrentTimestamp()
-        {
-            return DateTime.UtcNow.ToString(DateTimeFormat);
-        }
+    public static string GetCurrentTimestamp()
+    {
+        return DateTime.UtcNow.ToString(DateTimeFormat);
+    }
 
-        public static double SecondsSinceEpoch(DateTime dt)
-        {
-            return dt.Subtract(DateTime.UnixEpoch).TotalSeconds;
-        }
+    public static double SecondsSinceEpoch(DateTime dt)
+    {
+        return dt.Subtract(DateTime.UnixEpoch).TotalSeconds;
     }
 }
---
+++ EnoCore/Usings.cs
@@ -0,0 +1,17 @@
+﻿global using System;
+global using System.Buffers;
+global using System.Buffers.Binary;
+global using System.Buffers.Text;
+global using System.Collections.Generic;
+global using System.IO;
+global using System.Linq;
+global using System.Runtime.CompilerServices;
+global using System.Security.Cryptography;
+global using System.Text;
+global using System.Text.Json;
+global using System.Text.Json.Serialization;
+global using System.Threading.Channels;
+global using EnoCore.Models;
+global using EnoCore.Models.CheckerApi;
+global using EnoCore.Models.Database;
+global using Microsoft.Extensions.Logging;
