c408c81a800b71ae40cc9b46b55d9339e663214c
Benedikt Radtke <benediktradtke@gmail.com>
2021-11-15 09:44:26 +0100

    C# 10 EnoFlagSink

--- FlagShooter/FlagSubmissionClient.cs
+++ FlagShooter/FlagSubmissionClient.cs
@@ -1,73 +1,62 @@
-﻿namespace FlagShooter
+﻿namespace FlagShooter;
+
+internal class FlagSubmissionClient
 {
-    using System;
-    using System.IO;
-    using System.Net.Sockets;
-    using System.Text;
-    using System.Threading.Channels;
-    using System.Threading.Tasks;
-    using EnoCore;
-    using EnoCore.Models;
-    using EnoCore.Models.Database;
+    private readonly ChannelReader<byte[]> flagsReader;
+    private readonly TcpClient client;
 
-    internal class FlagSubmissionClient
+    private FlagSubmissionClient(ChannelReader<byte[]> flagsReader, TcpClient client)
     {
-        private readonly ChannelReader<byte[]> flagsReader;
-        private readonly TcpClient client;
+        this.flagsReader = flagsReader;
+        this.client = client;
+        Task.Run(this.Send);
+        Task.Run(this.Receive);
+    }
 
-        private FlagSubmissionClient(ChannelReader<byte[]> flagsReader, TcpClient client)
-        {
-            this.flagsReader = flagsReader;
-            this.client = client;
-            Task.Run(this.Send);
-            Task.Run(this.Receive);
-        }
+    public static async Task<FlagSubmissionClient> Create(ChannelReader<byte[]> flagsReader, long teamId, string address = "localhost")
+    {
+        var client = new TcpClient();
+        await client.ConnectAsync(address, 1338);
+        await client.Client.SendAsync(Encoding.ASCII.GetBytes($"{teamId}\n"), SocketFlags.None);
+        return new FlagSubmissionClient(flagsReader, client);
+    }
 
-        public static async Task<FlagSubmissionClient> Create(ChannelReader<byte[]> flagsReader, long teamId, string address = "localhost")
+    private async Task Receive()
+    {
+        StreamReader reader = new StreamReader(this.client.GetStream(), Encoding.ASCII);
+        try
         {
-            var client = new TcpClient();
-            await client.ConnectAsync(address, 1338);
-            await client.Client.SendAsync(Encoding.ASCII.GetBytes($"{teamId}\n"), SocketFlags.None);
-            return new FlagSubmissionClient(flagsReader, client);
-        }
-
-        private async Task Receive()
-        {
-            StreamReader reader = new StreamReader(this.client.GetStream(), Encoding.ASCII);
-            try
+            byte[] buf = new byte[2048];
+            while (true)
             {
-                byte[] buf = new byte[2048];
-                while (true)
+                string? result = await reader.ReadLineAsync();
+                if (result == null || result == string.Empty)
                 {
-                    string? result = await reader.ReadLineAsync();
-                    if (result == null || result == string.Empty)
-                    {
-                        throw new Exception($"result empty (connected={this.client.Client.Connected})");
-                    }
-
-                    throw new NotImplementedException("TODO adapt new reponses");
+                    throw new Exception($"result empty (connected={this.client.Client.Connected})");
                 }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine($"{nameof(this.Receive)} failed: {e.Message}");
+
+                throw new NotImplementedException("TODO adapt new reponses");
             }
         }
-
-        private async Task Send()
+        catch (Exception e)
         {
-            try
+            Console.WriteLine($"{nameof(this.Receive)} failed: {e.Message}");
+        }
+    }
+
+    private async Task Send()
+    {
+        try
+        {
+            while (true)
             {
-                while (true)
-                {
-                    var flag = await this.flagsReader.ReadAsync();
-                    await this.client.Client.SendAsync(flag, SocketFlags.None);
-                }
+                var flag = await this.flagsReader.ReadAsync();
+                await this.client.Client.SendAsync(flag, SocketFlags.None);
             }
-            catch (Exception e)
-            {
-                Console.WriteLine($"{nameof(this.Send)} failed: {e.Message}");
-            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"{nameof(this.Send)} failed: {e.Message}");
         }
     }
 }
